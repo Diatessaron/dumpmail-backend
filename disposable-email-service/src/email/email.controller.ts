8b86a3c5cc0fcffcc10d232f7375442372b9843c
Azat Fazlyev <afazlyev436@gmail.com>
2024-11-23 23:45:57 +0300

    minor fixes of return type in getEmail for one email

--- disposable-email-service/src/email/email.controller.ts
+++ disposable-email-service/src/email/email.controller.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import {EmailService} from './email.service';
 import {Controller, Get, Post, Query, Req, Res} from "@nestjs/common";
+import {Email} from "./model/email";
+import {ShortEmail} from "./dto/shortEmail";
 
 @Controller('/api/email')
 export class EmailController {
@@ -21,12 +23,12 @@ export class EmailController {
     }
 
     @Get('/all')
-    getEmails(@Query('email') emailAddress: string, @Query('page') page: number = 0) {
+    getEmails(@Query('email') emailAddress: string, @Query('page') page: number = 0): Promise<ShortEmail[]> {
         return this.emailService.getEmails(emailAddress, page);
     }
 
     @Get()
-    getEmail(@Query('email') emailAddress: string, @Query('date') date: string) {
+    getEmail(@Query('email') emailAddress: string, @Query('date') date: string): Promise<Email> {
         return this.emailService.getEmail(emailAddress, date);
     }
 }
