6f0ab632a1debf37f142089d0b1dc1703722f9d0
Azat Fazlyev <afazlyev436@gmail.com>
2024-12-03 21:24:46 +0300

    intermediate state of debugging the correct way to save emails

--- haraka/plugins/redis_store.js
+++ haraka/plugins/redis_store.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis');
-const simpleParser = require('mailparser').simpleParser;
 require('dotenv').config();
+const fs = require("fs");
 
 let redis = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
@@ -15,43 +15,60 @@ exports.hook_data = function (next, connection) {
 
 exports.hook_data_post = function (next, connection) {
     const transaction = connection.transaction;
-    const emailBody = transaction.body;
 
-    connection.loginfo("Body test: " + JSON.stringify(emailBody.bodytext))
+    let emailBody = transaction.body
 
-    simpleParser(emailBody.bodytext, (err, parsed) => {
+    fs.writeFile("./emailBody.txt", JSON.stringify(emailBody), function (err) {})
+
+    let headers = emailBody.header.headers;
+    const subject = headers.subject.map(el => el.substring(0, el.length - 1));
+    const from = headers.from.map(el => el.substring(0, el.length - 1));
+    const to = headers.to.map(el => el.substring(0, el.length - 1));
+
+    const parsedEmail = iterateChildren(emailBody)
+
+    fs.writeFile("./parsedEmail.txt", JSON.stringify(parsedEmail), function (err) {})
+
+    //todo: надо теперь тестировать в каком формате сохраняются реальные мэилы на серваке
+
+    const emailJson = {
+        subject: subject,
+        from: from,
+        to: to,
+        text: parsedEmail.body.text,
+        attachments: parsedEmail.attachments
+        // parsed.attachments.map(att => ({
+        // filename: att.filename || "Unnamed Attachment",
+        // contentType: att.contentType,
+        // size: att.size
+        // }))
+    };
+
+    const redisKey = `email:${to}:${Date.now()}`;
+    redis.set(redisKey, JSON.stringify(emailJson), 'EX', process.env.TTL, (err) => {
         if (err) {
-            connection.logerror(`Email parsing failed: ${err.message}`);
-            return next(DENYSOFT);
+            connection.logerror(`Redis .set() failed: ${err.message}`);
         }
-
-        connection.loginfo("Parsed text: " + JSON.stringify(parsed))
-        let headers = emailBody.header.headers;
-        const subject = headers.subject.map(el => el.substring(0, el.length - 1));
-        const from = headers.from.map(el => el.substring(0, el.length - 1));
-        const to = headers.to.map(el => el.substring(0, el.length - 1));
-
-        const emailJson = {
-            subject: subject,
-            from: from,
-            to: to,
-            text: emailBody.bodytext.substring(0, emailBody.bodytext.length - 1),
-            textAsHtml: parsed.textAsHtml,
-            isHtml: parsed.html,
-            attachments: parsed.attachments.map(att => ({
-                filename: att.filename,
-                contentType: att.contentType,
-                size: att.size
-            }))
-        };
-
-        const redisKey = `email:${to}:${Date.now()}`;
-        redis.set(redisKey, JSON.stringify(emailJson), 'EX', process.env.TTL, (err) => {
-            if (err) {
-                connection.logerror(`Redis .set() failed: ${err.message}`);
-            }
-            connection.loginfo(`Email stored successfully`);
-            next(OK)
-        });
+        connection.loginfo(`Email stored successfully`);
+        next(OK)
     });
 };
+
+function iterateChildren(emailBody) {
+    const children = emailBody.children
+    const parsedEmail = { body: {}, attachments: [] }
+
+    if (children.length === 0) {
+        parsedEmail.body.text = emailBody.bodytext
+    }
+
+    for (const child of children) {
+        if (child.state === "body") {
+            parsedEmail.body.text = child.bodytext;
+        } else if (child.state === "attachment") {
+            parsedEmail.attachments.push(Buffer.from(child.buf).toString())
+        }
+    }
+
+    return parsedEmail
+}
