7e9d7ce7a1718c62a5eb62e602dad18603d7b056
Azat Fazlyev <afazlyev436@gmail.com>
2024-12-04 19:13:42 +0300

    2. intermediate state of debugging the correct way to save emails

--- haraka/plugins/redis_store.js
+++ haraka/plugins/redis_store.js
@@ -1,12 +1,16 @@
 const Redis = require('ioredis');
-require('dotenv').config();
 const fs = require("fs");
+const path = require("path");
+const { extensions } = require("./extensions");
+require('dotenv').config();
 
 let redis = new Redis({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     lazyConnect: true
 });
+
+const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
 
 exports.hook_data = function (next, connection) {
     connection.transaction.parse_body = true;
@@ -18,18 +22,21 @@ exports.hook_data_post = function (next, connection) {
 
     let emailBody = transaction.body
 
+    //todo: удалить потом
     fs.writeFile("./emailBody.txt", JSON.stringify(emailBody), function (err) {})
 
     let headers = emailBody.header.headers;
     const subject = headers.subject.map(el => el.substring(0, el.length - 1));
     const from = headers.from.map(el => el.substring(0, el.length - 1));
-    const to = headers.to.map(el => el.substring(0, el.length - 1));
+    const to = headers.to.map(entry => {
+        const match = entry.match(emailRegex);
+        return match ? match[0] : null;
+    }).filter(Boolean);
 
     const parsedEmail = iterateChildren(emailBody)
 
+    //todo: удалить потом
     fs.writeFile("./parsedEmail.txt", JSON.stringify(parsedEmail), function (err) {})
-
-    //todo: надо теперь тестировать в каком формате сохраняются реальные мэилы на серваке
 
     const emailJson = {
         subject: subject,
@@ -37,11 +44,6 @@ exports.hook_data_post = function (next, connection) {
         to: to,
         text: parsedEmail.body.text,
         attachments: parsedEmail.attachments
-        // parsed.attachments.map(att => ({
-        // filename: att.filename || "Unnamed Attachment",
-        // contentType: att.contentType,
-        // size: att.size
-        // }))
     };
 
     const redisKey = `email:${to}:${Date.now()}`;
@@ -66,7 +68,30 @@ function iterateChildren(emailBody) {
         if (child.state === "body") {
             parsedEmail.body.text = child.bodytext;
         } else if (child.state === "attachment") {
-            parsedEmail.attachments.push(Buffer.from(child.buf).toString())
+            const contentTypeMatch = child.ct.match(/^([^;]+)/);
+            const nameMatch = child.ct.match(/name=\\"([^"]+)\\"/);
+            const contentType = contentTypeMatch ? contentTypeMatch[1] : null;
+            let fileName = nameMatch ? nameMatch[1] : null;
+            const file = Buffer.from(child.buf);
+            if (!path.extname(fileName)) fileName += extensions[contentType] || ".bin";
+
+            //todo: удалить потом
+            fs.writeFile(fileName, file, (err) => {
+                if (err) {
+                    console.error("Error writing the file:", err);
+                } else {
+                    console.log(`File saved as ${fileName}`);
+                }
+            });
+
+            const attachment = {
+                filename: fileName,
+                contentType: contentType,
+                size: file.length,
+                content: file.toString("base64")
+            }
+
+            parsedEmail.attachments.push(attachment)
         }
     }
 
