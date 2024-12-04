b0047b264c8758a43ea890f77e71c673549ea953
Azat Fazlyev <afazlyev436@gmail.com>
2024-12-04 22:32:48 +0300

    3. intermediate state of debugging the correct way to save emails

--- haraka/plugins/redis_store.js
+++ haraka/plugins/redis_store.js
@@ -22,8 +22,8 @@ exports.hook_data_post = function (next, connection) {
 
     let emailBody = transaction.body
 
-    //todo: удалить потом
-    fs.writeFile("./emailBody.txt", JSON.stringify(emailBody), function (err) {})
+    //in case of logging
+    // fs.writeFile("./emailBody.txt", JSON.stringify(emailBody), function (err) {})
 
     let headers = emailBody.header.headers;
     const subject = headers.subject.map(el => el.substring(0, el.length - 1));
@@ -33,10 +33,10 @@ exports.hook_data_post = function (next, connection) {
         return match ? match[0] : null;
     }).filter(Boolean);
 
-    const parsedEmail = iterateChildren(emailBody)
+    const parsedEmail = iterateChildren(emailBody, connection)
 
-    //todo: удалить потом
-    fs.writeFile("./parsedEmail.txt", JSON.stringify(parsedEmail), function (err) {})
+    //in case of logging
+    // fs.writeFile("./parsedEmail.txt", JSON.stringify(parsedEmail), function (err) {})
 
     const emailJson = {
         subject: subject,
@@ -45,6 +45,9 @@ exports.hook_data_post = function (next, connection) {
         text: parsedEmail.body.text,
         attachments: parsedEmail.attachments
     };
+
+    //in case of logging
+    // if (parsedEmail.attachments.length > 0) temp(parsedEmail.attachments[0])
 
     const redisKey = `email:${to}:${Date.now()}`;
     redis.set(redisKey, JSON.stringify(emailJson), 'EX', process.env.TTL, (err) => {
@@ -56,7 +59,7 @@ exports.hook_data_post = function (next, connection) {
     });
 };
 
-function iterateChildren(emailBody) {
+function iterateChildren(emailBody, connection) {
     const children = emailBody.children
     const parsedEmail = { body: {}, attachments: [] }
 
@@ -69,20 +72,14 @@ function iterateChildren(emailBody) {
             parsedEmail.body.text = child.bodytext;
         } else if (child.state === "attachment") {
             const contentTypeMatch = child.ct.match(/^([^;]+)/);
-            const nameMatch = child.ct.match(/name=\\"([^"]+)\\"/);
+            const nameMatch = child.ct.match(/name="([^"]+)"/);
             const contentType = contentTypeMatch ? contentTypeMatch[1] : null;
             let fileName = nameMatch ? nameMatch[1] : null;
             const file = Buffer.from(child.buf);
             if (!path.extname(fileName)) fileName += extensions[contentType] || ".bin";
 
-            //todo: удалить потом
-            fs.writeFile(fileName, file, (err) => {
-                if (err) {
-                    console.error("Error writing the file:", err);
-                } else {
-                    console.log(`File saved as ${fileName}`);
-                }
-            });
+            //in case of logging
+            // fs.writeFileSync("1" + fileName, file);
 
             const attachment = {
                 filename: fileName,
@@ -91,9 +88,30 @@ function iterateChildren(emailBody) {
                 content: file.toString("base64")
             }
 
+            //in case of logging
+            // fs.writeFileSync("2.txt", file.toString("base64"));
+
             parsedEmail.attachments.push(attachment)
         }
     }
 
     return parsedEmail
+}
+
+function temp(attachment) {
+    function saveBase64ToFile(base64Data, filePath) {
+        const base64Content = base64Data.startsWith("data:")
+            ? base64Data.split(",")[1]
+            : base64Data;
+
+        const fileBuffer = Buffer.from(base64Content, 'base64');
+
+        fs.writeFileSync(filePath, fileBuffer);
+        console.log(`File saved as ${filePath}`);
+    }
+
+    const fileName = "3" + attachment.filename;
+    const base64String = attachment.content;
+
+    saveBase64ToFile(base64String, fileName);
 }
