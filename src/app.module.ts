554c31bedd016841b9d2ba0796b2aa70f7e7d92f
Azat Fazlyev <afazlyev436@gmail.com>
2025-01-10 21:29:11 +0300

    Revert "A little cleaning"
    This reverts commit 416894f57f1db20afff7511638d0ce4db79901dc.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,24 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import {EmailController} from "./email/email.controller";
 import {JwtModule} from "@nestjs/jwt";
 import {EmailModule} from "./email/email.module";
 import {CookieParserMiddleware} from "./lib/CookieParserMiddleware";
 
 @Module({
-    imports: [
-        EmailModule,
-        JwtModule.register({
-            secret: process.env.JWT_SECRET,
-            signOptions: {expiresIn: '24h'},
-        }),
-    ],
-    controllers: [EmailController],
-    providers: [],
+  imports: [
+    EmailModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || "asd",
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  controllers: [AppController, EmailController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer): any {
-        consumer.apply(CookieParserMiddleware).forRoutes('*');
-    }
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(CookieParserMiddleware).forRoutes('*');
+  }
 }
