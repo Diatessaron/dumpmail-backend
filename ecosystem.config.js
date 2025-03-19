6b76bf831f543056e9d251c2e42c6cac7125d8bf
Azat Fazlyev <afazlyev436@gmail.com>
2025-03-19 21:54:23 +0400

    Changed config file for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: 'dumpmail-backend',
+      name: 'dumpmail-backend-1',
       script: 'npm',
       args: 'run start',
-      exec_mode: 'cluster',
-      instances: '2',
       autorestart: true,
       watch: false,
       env: {
+        PORT: 3001,
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'dumpmail-backend-2',
+      script: 'npm',
+      args: 'run start',
+      autorestart: true,
+      watch: false,
+      env: {
+        PORT: 3002,
         NODE_ENV: 'production',
       },
     },
